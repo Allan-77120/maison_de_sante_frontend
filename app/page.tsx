761be99bde79e59304f20b71146797c79c3de033
Your name <ramjaneshakeel@gmail.com>
2026-04-16 17:18:55 +0200

    savveee

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import Horaires from "@/components/Horaires";
 import Praticiens from "@/components/Praticiens";
 import MentionsLegales from "@/components/MentionsLegales";
 import Footer from "@/components/Footer";
-
+import Articles from "@/components/articles";
 export default function Home() {
   return (
     <main>
       <Hero />
       <Praticiens />
       <Horaires />
+      <Articles/> 
       <MentionsLegales />
       <Footer />
     </main>
